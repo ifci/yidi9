74cea3d97059a51d7e3aa2cfe447b143e8da4a57
ifci <i@ifci.cn>
2015-04-09 08:51:20 +0800

    2015/4/9 -02

--- js/sold.js
+++ js/sold.js
@@ -1,23 +1,24 @@
-(function($){
+$(function(){
   var val,url;
   $(".spec").on('click','.item:not(.sold-out)', function(event) {
-    $(this).toggleClass('selected').siblings('.item').removeClass('selected');
-    var spec = [];
-    for(var i = 0; i < $(".spec").length; i++){
-      // spec = '.yd_' + val;
-      val = $(".spec").eq(i).find('.selected').data('value');
-      // spec = spec + '_' + val;
-      spec.push(val);
-    }
-    url = $("#setImg li[data-key='" + spec + "']").children('.img').val();
-    $(".jqzoom").attr('src', url);
+      $(".attrEmpty").hide();
+      $(this).toggleClass('selected').siblings('.item').removeClass('selected');
+      var spec = [];
+      for(var i = 0; i < $(".spec").length; i++){
+        // spec = '.yd_' + val;
+          val = $(".spec").eq(i).find('.selected').data('value');
+        // spec = spec + '_' + val;
+          spec.push(val);
+      }
+      url = $("#setImg li[data-key='" + spec + "']").children('.img').val();
+      $(".jqzoom").attr('src', url);
 
-    var dv = $(this).data('value');
+      var dv = $(this).data('value');
 
-    //alert(String($("#setImg li[data-key='" + spec + "']")))
-    /*if(!($("#setImg li[data-key='" + spec + "']").length > 0)){
-      alert(1)
-    }*/
+      //alert(String($("#setImg li[data-key='" + spec + "']")))
+      /*if(!($("#setImg li[data-key='" + spec + "']").length > 0)){
+        alert(1)
+      }*/
 
       var arr = [];
       $("#setImg li[data-key*='" + dv + "']").each(function(index, el) {
@@ -27,28 +28,28 @@
       oArr = arr.join(',');
       oa = oArr.split(',');
 
-      /*console.log(oa);
-      console.log(dv);
-      console.log($.inArray(dv, oa));*/
+    /*console.log(oa);
+    console.log(dv);
+    console.log($.inArray(dv, oa));*/
       $(".spec .item").removeClass('sold-out');
       $(this).parents('.spec').siblings('.spec').find(".item").each(function(i, el) {
-          // $(this).removeClass('sold-out');
+            // $(this).removeClass('sold-out');
           var dval = $(this).data('value');
           if($.inArray(dval, oa) == '-1'){
-              // alert(dval);
+                // alert(dval);
               $(this).addClass('sold-out');
           }
       });
-
-
-    $(".sold-out").removeClass('selected');
-    if(!$(".selected").length){
-      $(".item").removeClass('sold-out');
-    }
-
   });
 
 
-
-
-})(jQuery);
+  $(".buyNub-buy").on('click', '.u-buy', function(event) {
+      $(".spec").each(function(index, el) {
+          var oId = $('.spec').eq(index);
+          var oSize = oId.find('.selected').length;
+          if(!oSize){
+              $(".attrEmpty").show();
+          }
+      });
+  });
+});
